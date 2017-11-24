760d38d8ed2628715cd51315b775df4ceadc59d6
jenksy <jjenksy@gmail.com>
2017-11-24 15:45:41 -0600

    completed wiring up my directive

--- app/app.js
+++ app/app.js
@@ -1,14 +1,56 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('ngJenksy', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+]);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+app.directive('arcGauge', function () {
+    return {
+        //template for directive
+        template: 'Name: {{customer.name}}<br /> Street: {{customer.street}}'
+    };
+});
+
+app.controller('CustomersController', ['$scope', function ($scope) {
+    var counter = 0;
+    $scope.customer = {
+        name: 'David',
+        street: '1234 Anywhere St.'
+    };
+
+    $scope.customers = [
+        {
+            name: 'David',
+            street: '1234 Anywhere St.'
+        },
+        {
+            name: 'Tina',
+            street: '1800 Crest St.'
+        },
+        {
+            name: 'Michelle',
+            street: '890 Main St.'
+        }
+    ];
+
+    $scope.addCustomer = function () {
+        counter++;
+        $scope.customers.push({
+            name: 'New Customer' + counter,
+            street: counter + ' Cedar Point St.'
+        });
+    };
+
+    $scope.changeData = function () {
+        counter++;
+        $scope.customer = {
+            name: 'James',
+            street: counter + ' Cedar Point St.'
+        };
+    };
 }]);
+
