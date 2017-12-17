a948eb1eaf9493152f028d760229d63287de119a
jenksy <jjenksy@gmail.com>
2017-12-17 10:52:29 -0600

    added value to the gauge

--- app/app.js
+++ app/app.js
@@ -19,6 +19,7 @@ app.directive('arcGauge', function () {
         template: '<div id="power-gauge"></div>',
         // templateUrl: 'mytemplate.html',
         link: function ($scope, element, attrs) {
+
             var gauge = function(container, configuration) {
                 var that = {};
                 var config = {
@@ -44,8 +45,7 @@ app.directive('arcGauge', function () {
                     majorTicks					: 5,
                     labelFormat					: d3.format('d'),
                     labelInset					: 10,
-
-                    arcColorFn					: d3.interpolateHsl(d3.rgb('#445467'), d3.rgb('#445467'))
+                    arcColorFn					: d3.rgb('#445467')
                 };
                 var range = undefined;
                 var r = undefined;
@@ -57,6 +57,7 @@ app.directive('arcGauge', function () {
                 var ticks = undefined;
                 var tickData = undefined;
                 var pointer = undefined;
+                var val = undefined;
 
 
                 function deg2rad(deg) {
@@ -64,10 +65,12 @@ app.directive('arcGauge', function () {
                 }
 
 
-                function configure(configuration) {
-                    var prop = undefined;
-                    for ( prop in configuration ) {
-                        config[prop] = configuration[prop];
+                function configure(configuration, pointerValue) {
+                    if (configuration  !== undefined) {
+                        var prop = undefined;
+                        for ( prop in configuration ) {
+                            config[prop] = configuration[prop];
+                        }
                     }
 
                     range = config.maxAngle - config.minAngle;
@@ -81,6 +84,7 @@ app.directive('arcGauge', function () {
                     ticks = scale.ticks(config.majorTicks);
                     tickData = d3.range(config.majorTicks).map(function() {return 1/config.majorTicks;});
 
+
                     arc = d3.arc()
                         .innerRadius(r - config.ringWidth - config.ringInset)
                         .outerRadius(r - config.ringInset)
@@ -92,6 +96,9 @@ app.directive('arcGauge', function () {
                             var ratio = d * (i+1);
                             return deg2rad(config.minAngle + (ratio * range));
                         });
+
+
+
                 }
                 that.configure = configure;
 
@@ -104,7 +111,7 @@ app.directive('arcGauge', function () {
                 }
                 that.isRendered = isRendered;
 
-                function render(newValue) {
+                function render(newValue, pointerValue) {
                     svg = d3.select(container)
                         .append('svg:svg')
                         .attr('class', 'gauge')
@@ -116,18 +123,23 @@ app.directive('arcGauge', function () {
                     var arcs = svg.append('g')
                         .attr('class', 'arc')
                         .attr('transform', centerTx);
-
+                    var d = 0;
                     arcs.selectAll('path')
                         .data(tickData)
                         .enter().append('path')
-                        .attr('fill', function(d, i) {
-                            return config.arcColorFn(d * i);
+                        .attr('fill', function(d2, i) {
+                            // the tick data to the d var
+                            d += d2;
+                            console.log(pointerValue);
+                                return config.arcColorFn;
                         })
                         .attr('d', arc);
+
 
                     var lg = svg.append('g')
                         .attr('class', 'label')
                         .attr('transform', centerTx);
+
 
                     lg.selectAll('text')
                         .data(ticks)
@@ -135,10 +147,33 @@ app.directive('arcGauge', function () {
                         .attr('transform', function(d) {
                             var ratio = scale(d);
                             var newAngle = config.minAngle + (ratio * range);
+
+                            if(d == 0){
+                                return 'rotate(' +newAngle +') translate(0,' +(config.labelInset +3 - r) +')';
+                                }
+
                             return 'rotate(' +newAngle +') translate(0,' +(config.labelInset - r) +')';
                         })
                         .text(config.labelFormat)
                         .attr('fill', config.textColor);
+
+                    //section for centered point value
+                    val = svg.append('g')
+                        .attr('class', 'val')
+                        .attr('transform', centerTx);
+                    console.log('attr', attrs.gaugeid);
+                    val.selectAll('text')
+                        .data([0])
+                        .enter()
+                        .append('text')
+                        .attr('class','pointValue')
+                        .attr('id', attrs.gaugeid)
+                        .attr('transform', function(d) {
+                            return 'rotate('+0+') translate(0,' + -10 +')';
+
+                        }).text(config.labelFormat)
+                        .attr('fill', config.textColor);
+
                     //M0.5,-65 L0,-135 L-0.5,-65 L0,-65 L0.5,-65
                     var lineData = [ [config.pointerWidth / 2, config.pointerOffset],
                         [0, -pointerHeadLength],
@@ -163,14 +198,13 @@ app.directive('arcGauge', function () {
                 }
                 that.render = render;
                 function update(newValue, newConfiguration) {
-                    if ( newConfiguration  !== undefined) {
-                        configure(newConfiguration);
-                    }
+                        configure(newConfiguration, newValue);
                     var ratio = scale(newValue);
                     var newAngle = config.minAngle + (ratio * range);
                     pointer.transition()
                         .duration(config.transitionMs)
                         .attr('transform', 'rotate(' +newAngle +')');
+                    d3.select("#"+attrs.gaugeid).text(newValue);
                 }
                 that.update = update;
 
@@ -178,21 +212,11 @@ app.directive('arcGauge', function () {
 
                 return that;
             };
-            // element.bind('click', function () {
-            //     element.html('You clicked me!');
-            // });
-            // element.bind('mouseenter', function () {
-            //     element.css('background-color', 'yellow');
-            // });
-            // element.bind('mouseleave', function () {
-            //     element.css('background-color', 'white');
-            // });
-
             //init the function
             function onDocumentReady() {
                 var powerGauge = gauge('#power-gauge', {
                     size: 300,
-                    clipWidth: 300,
+                    clipWidth: 350,//width of the canvas
                     clipHeight: 300,
                     ringWidth: 60,
                     maxValue: 100,
@@ -203,15 +227,16 @@ app.directive('arcGauge', function () {
                 powerGauge.render();
 
                 function updateReadings() {
-                    // just pump in random data here...
-                    powerGauge.update(Math.random() * 100);
+                    // just pump in random data here to fixed changes to the 100th p
+                    var num = (Math.random() * 100).toFixed(2);
+                    powerGauge.update(num);
                 }
 
                 // every few seconds update reading values
                 updateReadings();
-                // setInterval(function() {
-                //     updateReadings();
-                // }, 5 * 1000);
+                setInterval(function() {
+                    updateReadings();
+                }, 5 * 1000);
             }
 
             if ( !window.isLoaded ) {
